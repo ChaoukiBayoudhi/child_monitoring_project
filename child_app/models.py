91f9ac5cf9adea5b4f2a686b147ad417e0e40a7a
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-03-29 14:43:48 +0100

    Initial commit

---
+++ child_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from datetime import date
+# Create your models here.
+class Person(models.Model):
+    firstName=models.CharField(max_length=50,default='')
+    lastName=models.CharField(max_length=50,default='')
+    photo=models.ImageField(upload_to='person_photo',blank=True,null=True)
+    birthdate = models.DateField(default=date(1985,1,1))
+    class Meta:
+        abstract=True
+    def __str__(self):
+        #return self.firstName,', ',self.lastName,', ',self.birthdate
+        #or
+        #return f'firstName = {self.firstName}, lastName = {self.lastName}, birthdate = {self.birthdate}'
+        #or
+        return 'firstName = %s, lastName = %s, birthdate = %s' % self.firstName,self.lastName,self.birthdate
+class Parent(Person):
+    nb_childs=models.PositiveSmallIntegerField(default=1)
+    class Meta:
+        db_table='parent'
+    def __str__(self):
+        #return self.firstName,', ',self.lastName,', ',self.birthdate
+        #or
+        #return f'firstName = {self.firstName}, lastName = {self.lastName}, birthdate = {self.birthdate}'
+        #or
+        return 'firstName = %s, lastName = %s, birthdate = %s' % self.firstName,self.lastName,self.birthdate
+
+class Child(Person):
+    level=models.CharField(max_length=50,choices=[('1ST','first study class'),('2ND','second study class'),('3RD','third study class')],default='1ST')
