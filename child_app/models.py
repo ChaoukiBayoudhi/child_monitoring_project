c9f3c49b908a0adf5737f5cfde4c0bc5cd13dc53
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-04-19 09:37:40 +0100

    second commit

--- child_app/models.py
+++ child_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from datetime import date
+from django.utils import timezone
 # Create your models here.
+
+#define a django Enum
+#Defining a Django Enum <=> defining a new Type that have a limited set of values
+class TaskState(models.TextChoices):
+    NEW_TASK=('NEW','new task')
+    IN_PROGRESS_TASK=('IN_PROGRESS','task in progress')
+    DONE_TASK=('DONE','task done')
+    CANCELED_TASK=('CANCELED','task is aborted')
+    TO_DO_TASK=('TO_DO','task to do')
 class Person(models.Model):
     firstName=models.CharField(max_length=50,default='')
     lastName=models.CharField(max_length=50,default='')
@@ -17,14 +27,37 @@ class Person(models.Model):
 class Parent(Person):
     nb_chields=models.PositiveSmallIntegerField(default=1)
     class Meta:
-        db_table='parent'
+        db_table='parents'
     def __str__(self):
         #return self.firstName,', ',self.lastName,', ',self.birthdate
         #or
         #return f'firstName = {self.firstName}, lastName = {self.lastName}, birthdate = {self.birthdate}'
         #or
-        return 'firstName = %s, lastName = %s, birthdate = %s' % self.firstName,self.lastName,self.birthdate
-
+        #return 'firstName = %s, lastName = %s, birthdate = %s' % self.firstName,self.lastName,self.birthdate
+        return f'firstName = {self.firstName}, lastName = {self.lastName}'
 class Child(Person):
     level=models.CharField(max_length=50,choices=[('1ST','first study class'),('2ND','second study class'),('3RD','third study class')],default='1ST')
     parent=models.ForeignKey(Parent,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table='children'
+    def __str__(self):
+        return f'firstName = {self.firstName}, lastName = {self.lastName}'
+
+#define the model Task:
+class Task(models.Model):
+    name=models.CharField(max_length=50,default='',unique=True)
+    description=models.TextField(default='')
+    duration=models.DurationField(default=0)
+    start_date=models.DateTimeField(default=timezone.now)
+    end_date=models.DateTimeField(default=timezone.now)#+timezone.timedelta(days=1))
+    state=models.CharField(max_length=50,choices=TaskState.choices,default=TaskState.NEW_TASK)
+    #or
+    #state=models.CharField(max_length=50,choices=[('NEW','new task'),('IN_PROGRESS','task in progress'),('DONE''task done'),('CANCELED','task is aborted')],default='NEW')
+    task_type=models.CharField(max_length=50,choices=[('SPORT','sport task'),('HOMEWORK','homework task'),('HOBBY','hobby task'),('OTHER','Other task type')],default='TO_DO')
+    #relationships between task and child (many to one)
+    child=models.ForeignKey(Child,on_delete=models.CASCADE)
+    class Meta:
+        db_table='tasks'
+    def __str__(self):
+        return self.name
